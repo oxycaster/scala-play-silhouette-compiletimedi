b5b0722033c93e9543941d18bada555750b89cc8
oxycaster <480125+oxycaster@users.noreply.github.com>
2024-02-10 18:54:40 +0900

    よくわかっていないがSilhouetteで使用するEnvironmentの型としてEnvironment[I, A]な型定義が必要っぽいのでつくっといた

---
+++ app/DefaultEnv.scala
@@ -0,0 +1,8 @@
+import com.mohiva.play.silhouette.api.Env
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import domain.entity.ユーザー
+
+trait DefaultEnv extends Env {
+  type I = ユーザー
+  type A = CookieAuthenticator
+}
